2b99d767197441db6bddf8c94f9a9307915a0710
AnnPla <anna.platek2222@gmail.com>
2019-10-17 22:26:27 +0200

    Add functions: StartGame and Board

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,24 @@
-# 
+# functions
+def StartGame():
+    playing = input('Do you want to play tic tac toe game? (y/n) ')
+    if (playing == 'y'):
+        print('Start game!')
+        Board()
+    elif (playing == 'n'):
+        print('See you next time ;)')
+    else:
+        print('Please choose y or n')
+        StartGame()
+
+def Board():
+    player1_marker = input('Choose you marker (X or O): ')
+    if (player1_marker == 'X'):
+        player2_marker = 'O'
+    elif (player1_marker == 'O'):
+        player2_marker = 'X'
+    else:
+        print('Please choose X or O')
+        Board()
+    # todo: print board
+
+StartGame()
